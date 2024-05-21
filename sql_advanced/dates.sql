1319dafca199c1662e00aa60105feed719c16f9b
SamamaSaleem <msamama.saleem@gmail.com>
2024-05-21 21:30:17 +0500

    first commit

---
+++ sql_advanced/dates.sql
@@ -0,0 +1,8 @@
+SELECT 
+    job_title_short AS title,
+    job_location AS location,
+    job_posted_date AT TIME ZONE 'UTC' AT TIME ZONE 'EST' AS date,
+    EXTRACT(MONTH FROM job_posted_date) AS job_month,
+    EXTRACT(YEAR FROM job_posted_date) AS job_year
+FROM job_postings_fact
+LIMIT 5;
